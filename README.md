b498173e099217bac4ac72eb0435d8bde0960fb1
Andy Wang <climber2002@gmail.com>
2019-10-01 23:15:12 +1000

    Create integration tests and README

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a Ruby solution to the [DIUS bowling coding test](https://github.com/DiUS/coding-tests/blob/master/dius_bowling.md). It contains following folders,
+
+- lib: production code
+- spec: the RSpecs
+  - integration: Integration tests
+  - unit: Unit tests
+
+Please refer to `spec/integration/bowling_game_spec.rb` for the usage.
--- spec/integration/bowling_game_spec.rb
+++ spec/integration/bowling_game_spec.rb
@@ -1,5 +1,80 @@
 require 'bowling_game'
 
 describe BowlingGame do
-  
+  subject { described_class.new }
+
+  context 'when has a strike for every roll' do
+    before do
+      12.times { subject.roll(10) }
+    end
+
+    it 'has score 300' do
+      expect(subject.score).to eq 300
+    end
+  end
+
+  context 'when rolls some strikes and some spares, with last frame is a spare' do
+    before do
+      subject.roll(10)
+
+      subject.roll(7)
+      subject.roll(3)
+
+      subject.roll(7)
+      subject.roll(2)
+
+      subject.roll(9)
+      subject.roll(1)
+
+      subject.roll(10)
+
+      subject.roll(10)
+
+      subject.roll(10)
+
+      subject.roll(2)
+      subject.roll(3)
+
+      subject.roll(6)
+      subject.roll(4)
+
+      subject.roll(7)
+      subject.roll(3)
+      subject.roll(3)
+    end
+
+    it { expect(subject.score).to eq 168 }
+  end
+
+  context 'when rolls some strikes and some spares, with last frame is neither a spare nor a strike' do
+    before do
+      subject.roll(10)
+
+      subject.roll(7)
+      subject.roll(3)
+
+      subject.roll(7)
+      subject.roll(2)
+
+      subject.roll(9)
+      subject.roll(1)
+
+      subject.roll(10)
+
+      subject.roll(10)
+
+      subject.roll(10)
+
+      subject.roll(2)
+      subject.roll(3)
+
+      subject.roll(6)
+      subject.roll(4)
+
+      subject.roll(7)
+      subject.roll(2)
+    end
+
+    it { expect(subject.score).to eq 164 }
+  end
 end
